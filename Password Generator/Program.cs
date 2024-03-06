e033d7a8c2d986ccdca885e13b01ba8c4ce5f67c
beerking1 <honzabinder@post.cz>
2024-03-06 11:44:16 +0100

    init

---
+++ Password Generator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PassGen());
+            Console.ReadKey();
+        }
+        public static string PassGen()
+        {
+            string password = string.Empty;
+            string smallLetters = "abcdefghijklmnopqrstuvwxyz";
+            string bigLetters = "ABCDEGFGHIJKLMNOPQRSTUVWXYZ";
+            string numbers = "0123456789";
+            string specialChars = "/*-+´=)§¨$,.-_:?!'(ˇ%°;";
+            Random chars = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                password += numbers[chars.Next(numbers.Length)];
+                password += smallLetters[chars.Next(smallLetters.Length)];
+                password += bigLetters[chars.Next(bigLetters.Length)];                
+                password += specialChars[chars.Next(specialChars.Length)];
+            }
+            return $"Your new password is: {password}";
+        }
+    }
+}
